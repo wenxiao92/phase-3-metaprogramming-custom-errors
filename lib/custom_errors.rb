2c586cd38c544f0cae80ec4774c9942159c25f8b
Wen Xiao <wennstyle@gmail.com>
2022-06-09 00:19:10 -0400

    commit tree 3ba0a8ee7c69d10b96c26c9fd318cb4901e14325

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,14 +6,29 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
+  end
+
+  #defining the custom error class
+  class PartnerError < StandardError
+      def message
+        "you must give the get_married method an argument of an instance of the person class!"
+      end
   end
 
 end
 
 beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
+beyonce.get_married("Jay-Z") #error due to Jay-Z is a string and not an instance of the Person class
 puts beyonce.name
 
 
